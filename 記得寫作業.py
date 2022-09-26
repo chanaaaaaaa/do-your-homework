bab945251bb89ef3f809aa427b45b32828e6af2e
chanaaaaaaa <king960129@gmail.com>
2022-09-26 19:16:06 +0800

    Update 記得寫作業.py

--- 記得寫作業.py
+++ 記得寫作業.py
@@ -1,1 +1,10 @@
-hasattr
+import discord
+From discord.ext import commands
+
+bot = commands.Bot(command_prefix='+')
+
+@bot.event
+async deF on_ready():
+    print(">>bot is online <<")
+
+bot.run('MTAyMzkwOTEwOTIyMjg4NzQ5NQ.GbYThN.SacgsmY4xMk0W8o0ARrWcgIRhbibVO-HNa4_x8')
