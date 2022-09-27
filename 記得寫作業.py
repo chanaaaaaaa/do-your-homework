5e28dba5680457af139704fa6425e528ecc9fd08
chanaaaaaaa <king960129@gmail.com>
2022-09-27 21:37:42 +0800

    Update 記得寫作業.py

--- 記得寫作業.py
+++ 記得寫作業.py
@@ -1,10 +1,20 @@
 import discord
 from discord.ext import commands
+intents = discord.Intents.default()
 
-bot = commands.Bot(command_orefix='sd')
+bot = commands.Bot(command_prefix='+', intents = intents)
 
 @bot.event
 async def on_ready():
     print(">> Bot is online <<")
+
+@bot.event
+async def on_member_join(member):
+    print(f'[member]join!')
+
+@bot.event
+async def on_member_remove(member):
+    print(f'[member]leave!')
+
 
 bot.run('MTAyMzkwOTEwOTIyMjg4NzQ5NQ.GlHNp0.02Mc9jYOGfDUyZ5Y2i_urrtvvQvhzi9Li9re6Y')
