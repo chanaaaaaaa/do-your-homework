d49e77461aa03671a7543081963f287df9af37cd
chanaaaaaaa <king960129@gmail.com>
2022-09-28 20:59:28 +0800

    Update 記得寫作業.py

--- 記得寫作業.py
+++ 記得寫作業.py
@@ -1,7 +1,13 @@
 import discord
 from discord.ext import commands
+import json
+import random
+
 intents = discord.Intents.all()
 intents.members = True
+
+with open('.vscode\setting.json', mode = 'r',encoding="utf8") as jfile :
+    jdata = json.load(jfile)
 
 bot = commands.Bot(command_prefix='{', intents = intents)
 
@@ -11,16 +17,27 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
-    channel = bot.get_channel(1024318169483071558)
+    channel = bot.get_channel(int(jdata['welcome_channel']))
     await channel.send(f'{member}join!')
 
 @bot.event
 async def on_member_remove(member):
-    channel = bot.get_channel(1024318188504240178)
+    channel = bot.get_channel(int(jdata['leave_channel']))
     await channel.send(f'{member}leave!')
 
 @bot.command()
 async def ping(ctx):
     await ctx.send(f'{round(bot.latency*1000)}(ms)')
 
-bot.run('MTAyMzkwOTEwOTIyMjg4NzQ5NQ.G1yyjB.jO9t0tA_jIybr_MnNyif_4ngidTswMPukk929k')
+@bot.command()
+async def picture(ctx):
+    random_pic = random.choice(jdata['pic'])
+    pic = discord.File(random_pic)
+    await ctx.send(file = pic)
+
+@bot.command()
+async def web(ctx):
+    random_pic = random.choice(jdata['url_pic'])
+    await ctx.send(random_pic)
+
+bot.run(jdata['TOKEN'])
