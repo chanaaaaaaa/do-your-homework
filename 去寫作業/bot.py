3111c73ccecf41ead98deb59b256788c8989e68c
chanaaaaaaa <king960129@gmail.com>
2023-03-03 12:12:31 +0800

    Update bot.py

--- 去寫作業/bot.py
+++ 去寫作業/bot.py
@@ -1,28 +1,46 @@
 import discord
 from discord.ext import commands
 import json
-import os
+import asyncio
 
-intents = discord.Intents.all()
-intents.members = True
 
-bot = commands.Bot(command_prefix='=', intents = intents)
-
-with open('.\pic\setting.json', mode = 'r',encoding="utf8") as jfile :
+with open('.\setting.json', mode = 'r',encoding="utf8") as jfile :
     jdata = json.load(jfile)
 
-@bot.command()
-async def load(ctx,extension):
-    bot.load_extension(f'cogs.{extension}')
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='=', intents = intents)
+
+@bot.event
+async def on_ready():
+    print("機器啟動")
+
+@bot.event
+async def member_in(name):
+    await on_message(name)
 
 @bot.command()
-async def unload(ctx,extension):
-    bot.unload_extension(f'cogs.{extension}')
+async def hi(ctx):
+    await ctx.send('hi')
 
-for filename in os.listdir('./cmds'):
-    if filename.endswith('.py'):
-        bot.load_extension(f"cmds.{filename[:-3]}")
+@bot.command()
+async def channel(ctx):
+    await guild.creat_text_channel('測試',category=None)
 
+@bot.command()
+async def delete(ctx, channel: discord.TextChannel):
+    if ctx.author.guild.permissions.manage_channles:
+        await ctx.send.invoke(f"Deleted a channel named {name}")
+        await channel.delete()
+    else:
+       await ctx.send('Sorry, but you have a wrong name. Try again, but do it like this: >delete [channel name]') 
 
-if __name__=="__main__":
-    bot.run(jdata['TOKEN'])
+@bot.command()
+async def create(ctx, *, name=None):
+  guild = ctx.message.guild
+  if name == None:
+    await ctx.send('Sorry, but you have to insert a name. Try again, but do it like this: >create [channel name]')
+  else:
+    await guild.create_text_channel(name)
+    await ctx.send(f"Created a channel named {name}")
+
+bot.run(jdata['TOKEN'])
