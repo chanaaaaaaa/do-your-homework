7818919890ed3cce4854d3db0fd037d10f57a731
chanaaaaaaa <king960129@gmail.com>
2023-03-03 15:51:56 +0800

    Update bot.py

--- 去寫作業/bot.py
+++ 去寫作業/bot.py
@@ -1,8 +1,11 @@
-import discord
+import discord 
 from discord.ext import commands
 import json
 import asyncio
+import datetime
 from discord import Guild, TextChannel
+from discord.ext import tasks
+import asyncio
 
 
 with open('.\setting.json', mode = 'r',encoding="utf8") as jfile :
@@ -35,23 +38,61 @@ async def delete(ctx, channel: discord.TextChannel):
     else:
        await ctx.send('Sorry, but you have a wrong name. Try again, but do it like this: >delete [channel name]') 
 
+
 @bot.command()
-async def create(ctx, *, name=None):
-  guild = ctx.message.guild
-  if name == None:
-    await ctx.send('Sorry, but you have to insert a name. Try again, but do it like this: >create [channel name]')
-  else:
-    await guild.create_text_channel(name)
-    await ctx.send(f"Created a channel named {name}")
+async def createcc(ctx,name):
+    guild = ctx.guild
+    await guild.create_text_channel("{}".format(name))
+    
+
+
+   
+
+
+
+
 
 @commands.has_permissions(administrator=True)
+@bot.command()
+async def create_new_channel(ctx, channel_name: str):
+    guild = ctx.guild
+    existing_channel = discord.utils.get(guild.channels, name=channel_name)
+    admin_role = guild.get_role(956252661970247840)
+    overwrites = {
+        guild.default_role: discord.PermissionOverwrite(view_channel=True),
+        ctx.author: discord.PermissionOverwrite(view_channel=True),
+        admin_role: discord.PermissionOverwrite(view_channel=True),
+        guild.me: discord.PermissionOverwrite(view_channel=True)
+    }
+    if not existing_channel:
+        await guild.create_text_channel(channel_name ,overwrites=overwrites)
+        await ctx.send(f'Channel {channel_name} has been created!')
+    else:
+        await ctx.send(f'Channel {channel_name} already exists!')
+
+
+
+
 @bot.command()
 async def create_channel(ctx, channel_name: str):
     guild = ctx.guild
     existing_channel = discord.utils.get(guild.channels, name=channel_name)
     if not existing_channel:
-        print(f'Creating a new channel: {channel_name}')
         await guild.create_text_channel(channel_name)
         await ctx.send(f'成功：{channel_name}')
+
+
+
+
+
+@bot.command()
+async def date(ctx):
+    today = datetime.datetime.now().strftime("%Y-%m-%d")
+    message = f"今天是 {today}"
+    await ctx.send(message)
+
+
+
+
 
 bot.run(jdata['TOKEN'])
